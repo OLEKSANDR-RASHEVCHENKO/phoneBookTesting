ddccc8171fa2c8759ff1d88bf1d3326cda7d1f81
Pislaru <pislaru.daniel1@gmail.com>
2023-11-09 22:07:00 +0100

    Described FirstSeleniumTest

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+    public WebDriver driver;
+    @Test
+    public void firstSeleniumTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://phonebook.telran-edu.de:8080/user/login");
+        driver.manage().window().maximize();
+        driver.quit();
+    }
+
+}
