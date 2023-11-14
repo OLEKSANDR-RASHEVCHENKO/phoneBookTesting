9e84f1233081f7b1c062def611af5767d4f61a74
Pislaru <pislaru.daniel1@gmail.com>
2023-11-14 22:00:30 +0100

    Added class Locators.java and move all findElement to this class.

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -3,50 +3,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
 
 public class FirstSeleniumTest {
     public WebDriver driver;
     @Test
-    public void firstSeleniumTest(){
+    public void firstSeleniumTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://phonebook.telran-edu.de:8080/user/login");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //name
-        WebElement elementVariantNameOne = driver.findElement(By.xpath("//*[@name=\"email\"]"));
-        WebElement elementVariantNameTwo = driver.findElement(By.cssSelector("//*[@name=\"email\"]"));
-        WebElement elementVariantNameThree = driver.findElement(By.name("email"));
+        WebElement emailInput = driver.findElement(By.xpath("//*[@name='email']"));
+        emailInput.sendKeys("newtest@gmail.com");
 
-        // id
-        WebElement elementVariantOne = driver.findElement(By.xpath("//*[@id=`id=`login-form`]//*[@id=`defaultRegisterFormEmail`]"));
-        WebElement elementVariantTwo = driver.findElement(By.cssSelector("//*[@id=`defaultRegisterFormEmail`]"));
-        WebElement elementVariantThree = driver.findElement(By.cssSelector("@defaultRegisterFormEmail"));
-        WebElement elementVariantFour = driver.findElement(By.id("defaultRegisterFormEmail"));
+        WebElement passwordInput = driver.findElement(By.xpath("//*[@name='password']"));
+        passwordInput.sendKeys("newtest@gmail.com");
 
-        //class -bad practice
-        WebElement elementVariantClassOne = driver.findElement(By.xpath("//*[@id='login-form']//*[@class='form-control mb-2 rounded-pill ng-untouched ng-pristine ng-invalid']"));
-        WebElement elementVariantClassTwo = driver.findElement(By.xpath("//*[contains(@class, 'form-control mb-2')]"));
-        WebElement elementVariantClassThree = driver.findElement(By.cssSelector("[class='form-control mb-2 rounded-pill ng-untouched ng-pristine ng-invalid']"));
-        WebElement elementVariantClassFour = driver.findElement(By.cssSelector(".form-control.mb-2.rounded-pill.ng-untouched.ng-pristine.ng-invalid"));
-        WebElement elementVariantClassFive = driver.findElement(By.className("form-control mb-2 rounded-pill ng-untouched ng-pristine ng-invalid"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@type='submit']"));
+        loginButton.click();
+        Thread.sleep(10000);
 
-
-        WebElement elementVariantPasswordOne = driver.findElement(By.xpath("//*[@formcontrolname=`password`]"));
-        WebElement elementVariantPasswordTwo = driver.findElement(By.cssSelector("[formcontrolname=`password`]"));
-
-
-        WebElement elementVariantPlaceholderOne = driver.findElement(By.xpath("//*[@placeholder=`Password`]"));
-        WebElement elementVariantPlaceholderTwo = driver.findElement(By.cssSelector("[placeholder=`Password`"));
-
-
-        WebElement elementVariantNameOne1 = driver.findElement(By.xpath("//*[@name=`password`]"));
-        WebElement elementVariantNameTwo2 = driver.findElement(By.cssSelector("[name=`password`]"));
-        WebElement elementVariantNameThree3 = driver.findElement(By.name("password"));
-
-
-
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@class='list-group']"));
+        int actualContactCount = elements.size();
+        int expectedContactCount = 1009;
+        Assert.assertEquals(actualContactCount,expectedContactCount,actualContactCount + " not equal " + expectedContactCount);
         driver.quit();
     }
 
