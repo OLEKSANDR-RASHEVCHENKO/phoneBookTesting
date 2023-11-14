bb147aeaf93abfefba008ce351aca564b370aa54
Natalie Kovalchuk <koval4yk@hotmail.com>
2023-11-14 21:30:46 +0100

    Added class Locators.java and move all findElement to this class

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -13,39 +13,6 @@ public class FirstSeleniumTest {
         driver = new ChromeDriver();
         driver.get("http://phonebook.telran-edu.de:8080/user/login");
         driver.manage().window().maximize();
-        //name
-        WebElement elementVariantNameOne = driver.findElement(By.xpath("//*[@name='email']"));
-        WebElement elementVariantNameTwo = driver.findElement(By.cssSelector("[name='email']"));
-        WebElement elementVariantNameThree = driver.findElement(By.name("email"));
-
-        // id
-        WebElement elementVariantIdOne = driver.findElement(By.xpath("//*[@id='login-form']//*[@id ='defaultRegisterFormEmail']"));
-        WebElement elementVariantIdTwo = driver.findElement(By.cssSelector("[id='defaultRegisterFormEmail']"));
-        WebElement elementVariantIdThree = driver.findElement(By.cssSelector("#defaultRegisterFormEmail"));
-        WebElement elementVariantIdFour = driver.findElement(By.id("defaultRegisterFormEmail"));
-
-        //class -bad practice
-        WebElement elementVariantClassOne = driver.findElement(By.xpath("//*[@id='login-form']//*[@class='form-control mb-2 " +
-                "rounded-pill ng-untouched ng-pristine ng-invalid']"));
-        WebElement elementVariantClassTwo = driver.findElement(By.xpath("//*[contains(@class, 'form-control mb-2')]"));
-        WebElement elementVariantClassThree = driver.findElement(By.cssSelector("[class='form-control mb-2 rounded-pill ng-untouched ng-pristine ng-invalid']"));
-        WebElement elementVariantClassFour = driver.findElement(By.cssSelector(".form-control.mb-2.rounded-pill.ng-untouched.ng-pristine.ng-invalid"));
-        WebElement elementVariantClassFive = driver.findElement(By.className("form-control mb-2 rounded-pill ng-untouched ng-pristine ng-invalid"));
-
-        // by name
-        WebElement elementVariantByNameOne = driver.findElement(By.xpath("//*[@name='password']"));
-        WebElement elementVariantByNameTwo = driver.findElement(By.cssSelector("[name='password']"));
-        WebElement elementVariantByNameThree = driver.findElement(By.name("password"));
-
-
-        // form control
-        WebElement elementVariantFormControlOne = driver.findElement(By.xpath("//*[@formcontrolname='password']"));
-        WebElement elementVariantFormControlTwo = driver.findElement(By.cssSelector("[formcontrolname='password']"));
-        WebElement elementVariantFormControlThree = driver.findElement(By.name("form-control rounded-pill ng-untouched ng-pristine ng-invalid"));
-
-        //placeholder
-        WebElement elementVariantPlaceholderOne = driver.findElement(By.xpath("//*[@placeholder='Password']"));
-        WebElement elementVariantPlaceholderTwo = driver.findElement(By.cssSelector("[placeholder='Password']"));
 
         driver.quit();
     }
