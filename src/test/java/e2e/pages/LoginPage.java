5bcf03172824e111b4e0c632c6deccc81125b0d0
LeonadO555 <lmikhailov@supplyshift.net>
2023-11-17 00:35:59 +0400

    Described pages and LoginTest

---
+++ src/test/java/e2e/pages/LoginPage.java
@@ -0,0 +1,29 @@
+package e2e.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+    // important constructor!!!
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Describe locator
+    @FindBy(xpath = "//*[@name='email']")
+    WebElement emailInput;
+
+    @FindBy(xpath = "//*[@name='password']")
+    WebElement passwordInput;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    WebElement loginButton;
+
+    // Describe methods
+    public void login(String email, String password){
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+}
