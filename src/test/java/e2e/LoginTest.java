98ee869d9fa88938a34584d885a4049bca4067b0
AliaksandrPakhamovich <alexberr9501@gmail.com>
2023-11-16 21:36:10 +0100

    Described pages an LoginTest

---
+++ src/test/java/e2e/LoginTest.java
@@ -0,0 +1,22 @@
+package e2e;
+
+import e2e.pages.ContactsPage;
+import e2e.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase{
+    LoginPage loginPage;
+    ContactsPage contactsPage;
+    @Test
+    public void userCanLogin(){
+        String email = "newtest@gmail.com";
+        String password = "newtest@gmail.com";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email, password);
+
+        contactsPage = new ContactsPage(app.driver);
+        Assert.assertTrue(contactsPage.confirmLogin(),"User is not logged");
+    }
+}
---
+++ src/test/java/e2e/TestBase.java
@@ -0,0 +1,17 @@
+package e2e;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+    protected static ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod
+    public void setupTest(){
+        app.init();
+    }
+    @AfterMethod
+    public void tearDown(){
+        app.stop();
+    }
+}
