7d907107e4c8ba5411a1095e3669a6e26eca55fe
AliaksandrPakhamovich <alexberr9501@gmail.com>
2023-11-21 20:20:09 +0100

    Described ContactsPage

--- src/test/java/e2e/pages/AddContactDialog.java
+++ src/test/java/e2e/pages/AddContactDialog.java
@@ -9,5 +9,25 @@ public class AddContactDialog extends ContactsPage{
     public AddContactDialog(WebDriver driver) {
         super(driver);
     }
+    // Describe locator
+    @FindBy(xpath = "//*[@id='form-name']")
+    WebElement nameInput;
 
+    @FindBy(xpath = "//*[@id='form-lastName']")
+    WebElement lastNameInput;
+
+    @FindBy(xpath = "//*[@id='form-about']")
+    WebElement aboutInput;
+
+    @FindBy(xpath = "//*[@type='reset']")
+    WebElement cancelButton;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    WebElement saveButton;
+
+    @FindBy(xpath = "//*[@aria-label='Close']")
+    WebElement closeWindowsButton;
+
+    @FindBy(xpath = "//*[@id='form-error-firstName']")
+    WebElement errorMessage;
 }
--- src/test/java/e2e/pages/ContactInfoPage.java
+++ src/test/java/e2e/pages/ContactInfoPage.java
@@ -1,4 +1,33 @@
 package e2e.pages;
 
-public class ContactInfoPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactInfoPage extends ContactsPage{
+
+    public ContactInfoPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@id='contact-first-name']")
+    WebElement firstNameField;
+    @FindBy(xpath = "//div[@id='contact-last-name']")
+    WebElement lastNameField;
+    @FindBy(xpath = "//div[@id='contact-description']")
+    WebElement contactDescriptionField;
+    @FindBy(xpath = "//button[@id='btn-edit-contact']")
+    WebElement editButton;
+    @FindBy(xpath = "//button[@class='btn btn-secondary cancel-btn-ec']")
+    WebElement cancelButton;
+    @FindBy(xpath = "//button[@class='btn btn-primary submit-btn-ec']")
+    WebElement saveButton;
+
+    @FindBy(xpath = "//input[@name='input-ec-firstName']")
+    WebElement firstNameInput;
+    @FindBy(xpath = "//input[@name='input-ec-lastName]")
+    WebElement lastNameInput;
+    @FindBy(xpath = "//textarea[@name='input-ec-description']")
+    WebElement descriptionInput;
 }
+
