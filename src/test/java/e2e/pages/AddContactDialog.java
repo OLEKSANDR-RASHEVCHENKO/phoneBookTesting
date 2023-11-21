372d207486034b5d41a130f62c698accc119c085
Tetiana Skvarnyk <t.skvarnikgmail.com>
2023-11-21 22:02:09 +0100

    Described a lot of methods for a lot of classes

--- src/test/java/e2e/pages/AddContactDialog.java
+++ src/test/java/e2e/pages/AddContactDialog.java
@@ -3,20 +3,24 @@ package e2e.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class AddContactDialog extends ContactsPage{
     public AddContactDialog(WebDriver driver) {
         super(driver);
     }
     // Describe locator
+    @FindBy(xpath = "//*[@role='dialog']")
+    WebElement dialog;
+
     @FindBy(xpath = "//*[@id='form-name']")
-    WebElement nameInput;
+    WebElement firstNameInput;
 
     @FindBy(xpath = "//*[@id='form-lastName']")
     WebElement lastNameInput;
 
     @FindBy(xpath = "//*[@id='form-about']")
-    WebElement aboutInput;
+    WebElement descriptiontInput;
 
     @FindBy(xpath = "//*[@type='reset']")
     WebElement cancelButton;
@@ -29,5 +33,40 @@ public class AddContactDialog extends ContactsPage{
 
     @FindBy(xpath = "//*[@id='form-error-firstName']")
     WebElement errorMessage;
+
+    public void setFirstNameInput(String firstName){
+        setInput(firstNameInput, firstName);// заменяет 3 строки внизу
+
+        //firstNameInput.click(); //кликнуть -доступен для ввода
+        //firstNameInput.clear();
+        //firstNameInput.sendKeys(firstName);// ввод значения в инпут
+
+    }
+    public  void setLastNameInput(String lastName){
+        setInput(lastNameInput, lastName);
+
+        //lastNameInput.click();
+        //lastNameInput.clear();
+        //lastNameInput.sendKeys(lastName);
+
+    }
+
+    public void setDescription(String description){
+        setInput(descriptiontInput, description);
+
+        //descriptiontInput.click();
+        //descriptiontInput.clear();
+        //descriptiontInput.sendKeys(description);
+
+    }
+    public  void setAddContactForm(String firstName,String lastName, String description){
+        setFirstNameInput(firstName);
+        setLastNameInput(lastName);
+        setDescription(description);
+    }
+    public void SaveContact(){
+        saveButton.click();
+        Assert.assertFalse(isElementDisplayed(dialog), "Dialog is not closed");
+    }
 
 }
--- src/test/java/e2e/pages/ContactInfoPage.java
+++ src/test/java/e2e/pages/ContactInfoPage.java
@@ -17,7 +17,7 @@ public class ContactInfoPage extends ContactsPage{
     WebElement lastNameField;
 
     @FindBy(xpath = "//div[@id='contact-description']")
-    WebElement contactDescriptionField;
+    WebElement descriptionField;
 
     @FindBy(xpath = "//button[@id='btn-edit-contact']")
     WebElement editButton;
@@ -36,4 +36,25 @@ public class ContactInfoPage extends ContactsPage{
 
     @FindBy(xpath = "//textarea[@name='input-ec-description']")
     WebElement descriptionInput;
+
+    public  String getFirstName(){
+        return firstNameField.getText();
+    }
+
+    public  String getLastName(){
+        return lastNameField.getText();
+    }
+
+    public  String getDescription(){
+        return descriptionField.getText();
+    }
+
+    public void openEditContactForm(){
+        editButton.click();
+        isElementDisplayed(firstNameInput);
+    }
+
+    public void setFirstNameInput(String firstName){
+        setInput(firstNameInput, firstName);
+    }
 }
