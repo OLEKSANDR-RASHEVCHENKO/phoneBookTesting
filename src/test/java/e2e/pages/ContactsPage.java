02eb7e072ad491790d9c4db986db8030d7cb67b8
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2023-11-21 14:22:52 -0500

    Discribed @FindBy for ContactsPage.AddContactDialog. ContactInfoPage

--- src/test/java/e2e/pages/ContactsPage.java
+++ src/test/java/e2e/pages/ContactsPage.java
@@ -4,14 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ContactsPage extends BasePage{
+public class ContactsPage extends BasePage {
     public ContactsPage(WebDriver driver) {
         super(driver);
     }
+
     @FindBy(xpath = "//div[@class='collapse navbar-collapse']")
     WebElement header;
+    @FindBy(xpath = "//select[@id='langSelect']")
+    WebElement languageDropdown;
+    @FindBy(xpath = "//*[@id='contacts-list']")
+    WebElement contactsList;
 
-    public boolean confirmLogin(){
+    @FindBy(xpath = "//*[@formcontrolname='searchInput']")
+    WebElement searchInput;
+    @FindBy(xpath = "//*[@ng-reflect-router-link='/account']")
+    WebElement accountButton;
+
+
+    @FindBy(xpath = "//*[@src='/assets/icons/trash.svg']")
+    WebElement deleteButton;
+    @FindBy(xpath = "//*[text()='Logout']")
+    WebElement logoutButton;
+
+
+    public boolean confirmLogin() {
         return header.isDisplayed();
     }
 }
