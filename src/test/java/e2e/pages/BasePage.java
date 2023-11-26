473ac850408f4d3ec3ba8552d09398da31f0b9d7
nataliayankina <sstarget921@gmail.com>
2023-11-26 07:55:17 -0800

    Second commit

--- src/test/java/e2e/pages/BasePage.java
+++ src/test/java/e2e/pages/BasePage.java
@@ -1,14 +1,23 @@
 package e2e.pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
     public WebDriver driver;
 
-    public BasePage(WebDriver driver){
+    public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    protected boolean isElementDisplayed(WebElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 }
