770013ec1e1fa9d0d6bd6ddcfef179b6c53f1b94
Tetiana Skvarnyk <t.skvarnikgmail.com>
2023-12-05 22:05:48 +0100

    Added new class AddPhoneDialog.java and some methods

---
+++ src/test/java/e2e/pages/AddPhoneDialog.java
@@ -0,0 +1,48 @@
+package e2e.pages;
+
+import com.github.javafaker.PhoneNumber;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AddPhoneDialog extends PhonesPage {
+    public AddPhoneDialog(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id='cc-select']")
+    WebElement countryCode;
+
+    @FindBy(xpath = "//*[@for='selected-cc']")
+    WebElement phoneNumber;
+
+    @FindBy(xpath = "//*[@id='selected-cc']")
+    WebElement phoneNumberField;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    WebElement saveButton;
+
+    public void waitForOpen() {
+        getWait().forVisibility(countryCode);
+        getWait().forVisibility(phoneNumber);
+    }
+
+    public void selectCountryCode(String country) {
+        getSelect(countryCode).selectByVisibleText(country);
+    }
+
+    public String getLCountry() {
+        return getSelect(countryCode).getFirstSelectedOption().getText();
+
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber.getText();
+    }
+
+    public void setPhoneNumber(WebElement input, String value) {
+        input.click();
+        input.clear();
+        input.sendKeys(value);
+    }
+}
