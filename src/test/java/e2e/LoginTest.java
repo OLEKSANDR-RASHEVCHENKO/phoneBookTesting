c79fed22c74ca0b27a45e08c8a5e12719681e574
Liudmyla Anchuk <anchukliudmyla@gmail.com>
2023-12-06 21:30:24 +0100

    Described userCannotLoginWithInvalidEmail test

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -1,5 +1,6 @@
 package e2e;
 
+
 import e2e.pages.BasePage;
 import e2e.pages.ContactsPage;
 import e2e.pages.LoginPage;
@@ -23,5 +24,18 @@ public class LoginTest extends TestBase {
         contactsPage = new ContactsPage(app.driver);
 
         Assert.assertTrue(contactsPage.confirmLogin(),"User is not logged");
+    }
+
+    @Test
+    public void userCannotLoginWithInvalidEmail(){
+        String email = "newtest@gmail.com";
+        String password = "newtest@gmail.com";
+
+        loginPage = new LoginPage(app.driver);
+        loginPage.login(email,password);
+
+        contactsPage = new ContactsPage(app.driver);
+
+        Assert.assertFalse(contactsPage.confirmLogin(),"User is not logged");
     }
 }
