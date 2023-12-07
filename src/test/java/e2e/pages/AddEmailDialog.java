347b2ca66c6540ef1320b95515cc5a685692eaa7
Taras Krychkovskyi <krychkovski1@gmail.com>
2023-12-07 22:27:01 +0200

    updated wait, Added method  get Select inside have new object, select

---
+++ src/test/java/e2e/pages/AddEmailDialog.java
@@ -0,0 +1,75 @@
+package e2e.pages;
+
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AddEmailDialog extends EmailPage {
+    public AddEmailDialog(WebDriver driver) {
+        super(driver);
+
+    }
+
+    @FindBy(xpath = "//*[@role='dialog']")
+    WebElement dialog;
+
+    @FindBy(xpath = "//*[@id='input-email']")
+    WebElement addEmailButton;
+
+
+
+    @FindBy(xpath = "//*[@id='input-email']")
+    WebElement emailInput;
+
+
+    public void waitForOpen(){
+        getWait().forVisibility(dialog);
+        getWait().forVisibility(addEmailButton);
+        getWait().forVisibility(emailField);
+        getWait().forVisibility(emailInput);
+        getWait().forVisibility(saveButton);
+
+    }
+
+
+
+    public void setAddEmailButton(String addEmailButton)
+    {
+        setInput(addEmailButton);
+    }
+
+    public void setEmailField (String emailField){
+
+        setInput(openAddContactDialog().lastNameInput, emailField);
+    }
+
+
+    public void setLastNameInput(String lastName){
+
+        setInput(openAddContactDialog().lastNameInput, lastName);
+    }
+
+    public void setDescription(String description){
+
+        setInput(openAddContactDialog().descriptionInput, description);
+    }
+
+    public void setAddContactForm(String firstName, String lastName, String description){
+        setAddEmailButton(addEmailButton);
+        setLastNameInput(lastName);
+        setDescription(description);
+    }
+
+    public void saveContact(){
+        try {
+            getWait().forClickable(saveButton);
+            saveButton.click();
+            getWait().forInvisibility(dialog);
+        }catch (StaleElementReferenceException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
