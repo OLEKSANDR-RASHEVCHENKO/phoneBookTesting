6b5636fd824b961b66d28cf72b7cc23ebc86d4f6
AliaksandrPakhamovich <alexberr9501@gmail.com>
2023-12-14 20:31:15 +0100

    Added ImageMagick, config+

--- src/test/java/e2e/pages/BasePage.java
+++ src/test/java/e2e/pages/BasePage.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.awt.*;
 import java.awt.Dimension;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
@@ -45,30 +47,61 @@ public class BasePage {
         input.sendKeys(value);
     }
 
-    protected void takeAndCompareScreenshot(String actualScreenshotName, WebElement element) throws IOException {
-        String referenceImageFilePath = "reference/" + actualScreenshotName + ".png";
-        String tmpFilePath = "reference/tmp_" + actualScreenshotName + ".png";
+    private File takeScreenshot(WebElement element){
         File tmp;
         if (element == null){
             tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            System.out.println("Take screenshot page");
         }else {
             tmp = element.getScreenshotAs(OutputType.FILE);
+            System.out.println("Take screenshot element");
         }
+        return tmp;
+    }
+
+    private double calculateMaxDifferentPercentRation() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        return  0.01 * width * height;
+    }
+
+    private Process setCompareCommandToTerminal(String refImgFilePath, String tmpFilePath) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe", "compare", "-metric", "AE", refImgFilePath, tmpFilePath, "null:");
+        System.out.println("set compare command to terminal");
+        return pb.start();
+    }
+
+    protected void takeAndCompareScreenshot(String actualScreenshotName, WebElement element) throws IOException {
+        String referenceImageFilePath = "reference/" + actualScreenshotName + ".png";
+        String tmpFilePath = "reference/tmp_" + actualScreenshotName + ".png";
+        File tmp = takeScreenshot(element);
 
         Files.copy(tmp.toPath(),new File(tmpFilePath).toPath(), StandardCopyOption.REPLACE_EXISTING);
 
         File referenceImageFile = new File(referenceImageFilePath);
         if (!referenceImageFile.exists()){
-            throw new RuntimeException("Reference image file does not exist" +  referenceImageFilePath);
+            throw new RuntimeException("Reference image file does not exist, but there is tmp file, need remove tmp_ from name file" +  tmpFilePath);
         }
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = screenSize.width;
-        int height = screenSize.height;
+        double maxDiffPercent = calculateMaxDifferentPercentRation();
+        Process process = setCompareCommandToTerminal(referenceImageFilePath, tmpFilePath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-        double maxDiffPercent = 0.01 * width * height;
+        String line;
+        double difference = 0;
+        while ((line = reader.readLine()) != null){
+            difference = Integer.parseInt(line.trim());
+        }
+        reader.close();
+        process.destroy();
 
-        ProcessBuilder pb = new ProcessBuilder("C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\compare.exe !!!", "-metric", "AE", referenceImageFilePath, tmpFilePath, "null:");
+        if (difference > maxDiffPercent){
+            throw new RuntimeException(referenceImageFilePath + " not equal " + tmpFilePath + " difference: " + difference);
+        }
+
+        Files.deleteIfExists(new File(tmpFilePath).toPath());
     }
 
 
