dcdd2979f3170421d7c3e62997b88e400b36d35e
LeonadO555 <lmikhailov@supplyshift.net>
2023-12-22 22:12:22 +0400

    Added new reference images , added loginTestMethod

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -6,29 +6,63 @@ import e2e.utils.DataProviders;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest extends TestBase{
+public class LoginTest extends TestBase {
     LoginPage loginPage;
     ContactsPage contactsPage;
 
     @Test
-    public void userCanLogin(){
+    public void userCanLogin() {
         String email = "newtest@gmail.com";
         String password = "newtest@gmail.com";
 
         loginPage = new LoginPage(app.driver);
-        loginPage.login(email,password);
+        loginPage.login(email, password);
 
         contactsPage = new ContactsPage(app.driver);
         contactsPage.waitForLoading();
     }
 
     @Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
-    public void userCannotLogin(String email, String password, String caseName){
+    public void userCannotLogin(String email, String password, String caseName) {
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
-        loginPage.login(email,password);
+        loginPage.login(email, password);
 
         loginPage.waitForLoading();
         loginPage.takeLoginPageScreenshot(caseName + "_negative_login_case");
+    }
+
+    private void loginTestMethod(String email, String password, String screenshotName, boolean negativeCase) {
+        loginPage = new LoginPage(app.driver);
+        loginPage.waitForLoading();
+        loginPage.login(email, password);
+        if (negativeCase) {
+            loginPage.waitForLoading();
+            loginPage.takeLoginPageScreenshot(screenshotName);
+        } else {
+            contactsPage = new ContactsPage(app.driver);
+            contactsPage.waitForLoading();
+            contactsPage.takeScreenshotHeader();
+        }
+    }
+
+    @Test
+    public void userCanLoginWithValidData() {
+        loginTestMethod("newtest@gmail.com", "newtest@gmail.com", null, false);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidEmail() {
+        loginTestMethod("newtestgmail.com", "newtest@gmail.com", "login_invalid_email", true);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidPassword() {
+        loginTestMethod("newtest@gmail.com", "newtestgmail.com", "login_invalid_password", true);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidEmailAndPassword() {
+        loginTestMethod("newtestgmail.com", "newtestgmail.com", "login_invalid_email_and_password", true);
     }
 }
--- src/test/java/e2e/pages/ContactsPage.java
+++ src/test/java/e2e/pages/ContactsPage.java
@@ -82,6 +82,10 @@ public class ContactsPage extends BasePage {
         return isElementDisplayed(noResultsMessage);
     }
 
+    public void takeScreenshotHeader(){
+        takeAndCompareScreenshot("header", header);
+    }
+
     public void takeScreenshotNoResultMessage(){
         takeAndCompareScreenshot("contactsPageNoResultMessage", noResultsMessage);
     }
