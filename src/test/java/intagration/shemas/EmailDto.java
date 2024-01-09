b82ee28c4fdbb8ba9f0887d0c625cef27a0b8689
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-01-09 18:08:50 -0500

    added ApiBase , ContactApi,ContactDto, EmailDto. Intagration

---
+++ src/test/java/intagration/shemas/EmailDto.java
@@ -0,0 +1,49 @@
+package intagration.shemas;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+public class EmailDto {
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("contact")
+    private String contact;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
+
+
+
