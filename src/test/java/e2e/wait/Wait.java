9fefc50ecc19cac38645a312bc3d81c42d72dd37
nata757 <107883642+nata757@users.noreply.github.com>
2024-01-09 20:05:20 +0100

    Api test

--- src/test/java/e2e/wait/Wait.java
+++ src/test/java/e2e/wait/Wait.java
@@ -1,4 +1,4 @@
-package e2e.wait;
+package e2e.Wait;
 
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -9,46 +9,47 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-public class Wait{
+public class Wait {
     public WebDriver driver;
+
     Duration TIMEOUT = Duration.ofSeconds(10);
 
-    public Wait(WebDriver driver){
-        this.driver= driver;
+    public Wait(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public WebDriverWait setWait(){
-        return new WebDriverWait(driver,TIMEOUT);
+    public WebDriverWait setWait() {
+        return new WebDriverWait(driver, TIMEOUT);
     }
 
-    public void forVisibility(WebElement element){
+    public void forVisibility(WebElement element) {
         try {
             setWait().until(ExpectedConditions.visibilityOf(element));
-        } catch (TimeoutException e){
+        } catch (TimeoutException e) {
             throw new TimeoutException(element.getAccessibleName() + " is not visible more than " + TIMEOUT.toString());
         }
     }
 
-    public void forAllVisibility(List<WebElement> elements){
+    public void forAllVisibility(List<WebElement> elements) {
         try {
             setWait().until(ExpectedConditions.visibilityOfAllElements(elements));
-        } catch (TimeoutException e){
+        } catch (TimeoutException e) {
             throw new TimeoutException(elements.size() + " are not visible more than " + TIMEOUT.toString());
         }
     }
 
-    public void forInvisibility(WebElement element){
+    public void forInvisibility(WebElement element) {
         try {
             setWait().until(ExpectedConditions.invisibilityOf(element));
-        } catch (TimeoutException e){
+        } catch (TimeoutException e) {
             throw new TimeoutException(element.getAccessibleName() + " is visible more than " + TIMEOUT.toString());
         }
     }
 
-    public void forClickable(WebElement element){
+    public void forClickable(WebElement element) {
         try {
             setWait().until(ExpectedConditions.elementToBeClickable(element));
-        } catch (TimeoutException e){
+        } catch (TimeoutException e) {
             throw new TimeoutException(element.getAccessibleName() + " is not clickable more than " + TIMEOUT.toString());
         }
     }
